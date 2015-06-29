5418788b524d341c52ae7790734b9304ad3aae73
Shubham Mazumder <flatmath@users.noreply.github.com>
2015-06-29 17:28:44 +0530

    Update image-url cdn

--- 4chan.sh
+++ 4chan.sh
@@ -1,4 +1,4 @@
 #4chan wget script
 echo "Enter URL: "
 read URL
-wget -H --no-directories -D images.4chan.org -r -l 1 -U Mozilla -A jpeg,jpg,png,gif -nc --wait 3 --random-wait -e robots=off $URL
+wget -H --no-directories -P 4chan-pics -r -Di.4cdn.org -r -l 1 -U Mozilla -A ".jpg,.jpeg,.png,.gif," -nc --wait 3 --random-wait -e robots=off $URL
