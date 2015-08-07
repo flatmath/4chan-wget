1fc3eaae83c0527906c71e3888a5c7e91a79a98b
Shubham Mazumder <shubham.mazumder@gmail.com>
2015-08-08 00:21:12 +0530

    Add User-defined Folder Name

--- 4chan.sh
+++ 4chan.sh
@@ -1,4 +1,6 @@
 #4chan wget script
 echo "Enter URL: "
 read URL
-wget -H --no-directories -P 4chan-pics -r -Di.4cdn.org -r -l 1 -U Mozilla -A ".jpg,.jpeg,.png,.gif," -nc --wait 3 --random-wait -e robots=off $URL
+echo "Enter FOLDER NAME: "
+read  FOLDER
+wget -H --no-directories -P $FOLDER -r -Di.4cdn.org -r -l 1 -U Mozilla -A ".jpg,.jpeg,.png,.gif," -nc --wait 3 --random-wait -e robots=off $URL
